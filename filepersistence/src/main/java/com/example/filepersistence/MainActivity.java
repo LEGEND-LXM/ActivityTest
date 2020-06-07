7d4f625bdc1d0fb0b451cda7a105d6d5cbf2f01d
LEGEND-LXM <2748346961@qq.com>
2020-06-07 22:13:57 +0800

    文件数据存储学习完成

--- filepersistence/src/main/java/com/example/filepersistence/MainActivity.java
+++ filepersistence/src/main/java/com/example/filepersistence/MainActivity.java
@@ -2,13 +2,92 @@ package com.example.filepersistence;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.edit);
+        String text = loadFile();
+        if (!TextUtils.isEmpty(text)) {
+            editText.setText(text);
+            editText.setSelection(text.length());
+        }
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        String input = editText.getText().toString();
+        saveText(input);
+    }
+
+    public void saveText(String input) {
+        //对FileOutputStream对象和BufferedWriter对象进行清空
+        FileOutputStream out = null;
+        BufferedWriter writer = null;
+        try {
+            //对FileOutputStream对象和BufferedWriter对象进行初始化
+            out = openFileOutput("data", Context.MODE_PRIVATE);
+            writer = new BufferedWriter(new OutputStreamWriter(out));
+            //写入数据
+            writer.write(input);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                //清空writer
+                if (writer != null) {
+                    writer.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String loadFile() {
+        FileInputStream in = null;
+        BufferedReader reader = null;
+        StringBuilder content = new StringBuilder();
+        try {
+            in = openFileInput("data");
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                content.append(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return content.toString();
     }
 }
