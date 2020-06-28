911617333ac2443f612f0c8093acc973a3dd4fc1
LEGEND-LXM <2748346961@qq.com>
2020-06-28 09:46:23 +0800

    相机拍照保存完成

--- cameraalbum/src/main/java/com/example/cameraalbum/MainActivity.java
+++ cameraalbum/src/main/java/com/example/cameraalbum/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.cameraalbum;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
+import androidx.core.os.EnvironmentCompat;
 
+import android.Manifest;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
@@ -16,14 +23,27 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    // 是否是Android 10以上手机
+    private boolean isAndroidQ = Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q;
+
+    //用于保存拍照图片的uri
+    private Uri mCameraUri;
+
+    // 用于保存图片的文件路径，Android 10以下使用图片路径访问图片
+    private String mCameraImagePath;
+
+    private static final int PERMISSION_CAMERA_REQUEST_CODE = 2;
 
     public static final int REQUEST_IMAGE_CAPTURE = 1;  // 被启动活动的ID
     private String currentPhotoPath;  // 存储图片保存的路径
@@ -39,10 +59,10 @@ public class MainActivity extends AppCompatActivity {
         takePhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                dispatchTakePictureIntent();
+                checkPermissionAndCamera();
             }
         });
-
+        Log.d("MainActivity", "onCreate succeed");
     }
 
     /**
@@ -57,54 +77,132 @@ public class MainActivity extends AppCompatActivity {
         switch (requestCode) {
             case REQUEST_IMAGE_CAPTURE:
                 if (resultCode == RESULT_OK) {
-                    Bundle bundle = data.getExtras();
-                    Bitmap imageBitmap = (Bitmap) bundle.get("data");
-                    picture.setImageBitmap(imageBitmap);
+                    if (isAndroidQ) {
+                        // Android 10 使用图片uri加载
+                        picture.setImageURI(mCameraUri);
+                    } else {
+                        // 使用图片路径加载
+                        picture.setImageBitmap(BitmapFactory.decodeFile(mCameraImagePath));
+                    }
+                } else {
+                    Toast.makeText(this, "取消", Toast.LENGTH_LONG).show();
                 }
+                break;
+            default:
+                break;
         }
     }
+
+    /**
+     * 申请运行时函数的回调函数
+     * */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_CAMERA_REQUEST_CODE:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //允许权限，有调起相机拍照。
+                    dispatchTakePictureIntent();
+                } else {
+                    //拒绝权限，弹出提示框。
+                    Toast.makeText(this,"拍照权限被拒绝",Toast.LENGTH_LONG).show();
+                }
+                break;
+            default:
+                break;
+        }
+        Log.d("MainActivity", "onRequestPermissionsResult succeed");
+    }
+
+    /**
+     * 创建图片地址uri,用于保存拍照后的照片 Android 10以后使用这种方法
+     */
+    private Uri createImageUri() {
+        String status = Environment.getExternalStorageState();
+        // 判断是否有SD卡,优先使用SD卡存储,当没有SD卡时使用手机存储
+        if (status.equals(Environment.MEDIA_MOUNTED)) {
+            return getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, new ContentValues());
+        } else {
+            return getContentResolver().insert(MediaStore.Images.Media.INTERNAL_CONTENT_URI, new ContentValues());
+        }
+    }
+
     /**
      * 此函数作用为 ：为照片创建文件
-     * getExternalFilesDir() ：创建一个私有文件夹
+     * getExternalFilesDir() ：创建一个共享文件夹
      * */
     private File createImageFile() throws IOException {
         // 创建一个唯一的 image 名称
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
+        String imageName = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        // Save a file: path for use with ACTION_VIEW intents
-        currentPhotoPath = image.getAbsolutePath();
-        return image;
+        if (!storageDir.exists()) {
+            storageDir.mkdir();
+        }
+        File tempFile = new File(storageDir, imageName);
+        if (!Environment.MEDIA_MOUNTED.equals(EnvironmentCompat.getStorageState(tempFile))) {
+            return null;
+        }
+        return tempFile;
     }
 
     /**
      * 此函数作用为打开相机
      * takePictureIntent.resolveActivity() ：判断takePictureIntent是否能被处理，如果不能处理
      * 强制使用此Intent对象的话会使程序崩溃
-     * photoFile = createImageFile(); : 获取图片的唯一文件名
+     * photoFile = createImageFile(); : 获取图片文件路径名
+     * 此处要注意 ：android10以下用图片路径访问保存，android10及以上用图片uri来访问
      * */
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             File photoFile = null;
-            try {
-                photoFile = createImageFile();
-                Log.d("MainActivity", photoFile.toString());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this, "com.example.android.provider", photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            Uri photoUri = null;
+
+            if (isAndroidQ) {
+                // 适配android 10
+                photoUri = createImageUri();
+            } else {
+                try {
+                    photoFile = createImageFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (photoFile != null) {
+                    mCameraImagePath = photoFile.getAbsolutePath();
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                        //适配Android 7.0文件权限，通过FileProvider创建一个content类型的Uri
+                        photoUri = FileProvider.getUriForFile(this, getPackageName() + ".fileprovider", photoFile);
+                    } else {
+                        photoUri = Uri.fromFile(photoFile);
+                    }
+                }
             }
 
+            mCameraUri = photoUri;
+            if (photoUri != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                takePictureIntent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
         }
+    }
+
+    /**
+     * 相机运行时权限获取
+     * */
+    private void checkPermissionAndCamera() {
+        int hasCameraPermission = ContextCompat.checkSelfPermission(getApplication(),
+                Manifest.permission.CAMERA);
+        if (hasCameraPermission == PackageManager.PERMISSION_GRANTED) {
+            //有调起相机拍照。
+            dispatchTakePictureIntent();
+        } else {
+            //没有权限，申请权限。
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.CAMERA},
+                    PERMISSION_CAMERA_REQUEST_CODE);
+        }
+        Log.d("MainActivity", "checkPermissionAndCamera succeed");
     }
 }
