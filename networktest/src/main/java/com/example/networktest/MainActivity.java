39772f357f5085361c37ae1845658a5dcc23c902
LEGEND-LXM <2748346961@qq.com>
2020-08-12 12:18:51 +0800

    HTTP学习完毕

--- networktest/src/main/java/com/example/networktest/MainActivity.java
+++ networktest/src/main/java/com/example/networktest/MainActivity.java
@@ -2,19 +2,35 @@ package com.example.networktest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.XmlResourceParser;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+
+import okhttp3.Call;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,12 +44,100 @@ public class MainActivity extends AppCompatActivity {
         Button sendBtn = findViewById(R.id.sendRequestBtn);
         textView = findViewById(R.id.responseText);
 
+
+
         sendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendRequestWithHttpURLConnection();
+//                sendRequestWithHttpURLConnection();  // 使用HttpURLConnection
+                sendRequestWithOkHttp();  // 使用OkHttp
             }
         });
+    }
+
+    private void sendRequestWithOkHttp() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                OkHttpClient client = new OkHttpClient();
+                RequestBody requestBody = new FormBody.Builder()
+                        .add("username", "admin")
+                        .add("password", "123456")
+                        .build();
+
+                Request request = new Request.Builder()
+                        .url("http://192.168.9.103/get_data.xml")
+                        .build();
+                try {
+                    Response response = client.newCall(request).execute();
+                    String responseData = response.body().string();
+                    if (responseData != null) {
+//                        showResponse(responseData);
+                        parseXMLWithPull(responseData);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    private void pareseJSONWithJSONObject(String jsonData) throws JSONException {
+        JSONArray jsonArray = new JSONArray(jsonData);
+        for (int i = 0; i<jsonArray.length(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            String id = jsonObject.getString("id");
+            String name = jsonObject.getString("name");
+            String version = jsonObject.getString("version");
+            Log.d("MainActivity", "id"+id);
+            Log.d("MainActivity", "name"+name);
+            Log.d("MainActivity", "version"+version);
+
+        }
+    }
+
+    private void parseXMLWithPull(String xmlData) {
+        try {
+            // 获取XmlPullParserFactory实例
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            // 获取XmlPullParser对象
+            XmlPullParser xmlPullParser = factory.newPullParser();
+            // 将获取的 XML数据设置进去
+            xmlPullParser.setInput(new StringReader(xmlData));
+            // 得到当前解析的事件getEventType()
+            int eventType = xmlPullParser.getEventType();
+            String id = "";
+            String name = "";
+            String version = "";
+            while (eventType != xmlPullParser.END_DOCUMENT) {  // 判断解析工作是否完成
+                String nodeName = xmlPullParser.getName();  // 获取当前节点的名字
+                switch (eventType) {
+                    // 开始解析某个节点
+                    case XmlPullParser.START_TAG :
+                        if ("id".equals(nodeName)) {
+                            id = xmlPullParser.nextText();  // 获取节点的具体内容
+                        } else if ("name".equals(nodeName)) {
+                            name = xmlPullParser.nextText();
+                        } else if ("version".equals(nodeName)) {
+                            version = xmlPullParser.nextText();
+                        }
+                        break;
+                    // 完成解析某个点
+                    case XmlPullParser.END_TAG:
+                        if ("app".equals(nodeName)) {
+                            Log.d("MainActivity","id:"+id);
+                            Log.d("MainActivity","name:"+name);
+                            Log.d("MainActivity","version:"+version);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                eventType = xmlPullParser.next();  // 获取下一个解析事件
+            }
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void sendRequestWithHttpURLConnection() {
---
+++ networktest/src/main/java/com/example/networktest/MyHandler.java
@@ -0,0 +1,62 @@
+package com.example.networktest;
+
+import android.util.Log;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class MyHandler extends DefaultHandler{
+
+    private String nodeName;
+    private StringBuilder id;
+    private StringBuilder name;
+    private StringBuilder version;
+
+    @Override
+    public void startDocument() throws SAXException {
+        super.startDocument();
+        id = new StringBuilder();
+        name = new StringBuilder();
+        version = new StringBuilder();
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        super.startElement(uri, localName, qName, attributes);
+        // 记录当前节点
+        nodeName = localName;
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        super.characters(ch, start, length);
+        // 根据当前的节点名来判断将内容添加到哪一个StringBuilder对象中
+        if ("id".equals(nodeName)) {
+            id.append(ch, start, length);
+        } else if ("name".equals(nodeName)) {
+            name.append(ch, start, length);
+        } else if ("version".equals(nodeName)) {
+            version.append(ch, start, length);
+        }
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        super.endElement(uri, localName, qName);
+        if ("app".equals(nodeName)) {
+            Log.d("MyHandler", "id"+id.toString().trim());
+            Log.d("MyHandler", "name"+name.toString().trim());
+            Log.d("MyHandler", "version"+version.toString().trim());
+            // 最后要清空StringBuilder
+            id.setLength(0);
+            name.setLength(0);
+            version.setLength(0);
+        }
+    }
+
+    @Override
+    public void endDocument() throws SAXException {
+        super.endDocument();
+    }
+}
