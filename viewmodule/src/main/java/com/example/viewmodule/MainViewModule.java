60e3fd42c5678daac7d0cad226e3df149983db60
LEGEND-LXM <52842831+LEGEND-LXM@users.noreply.github.com>
2020-11-25 23:44:04 +0800

    更新 ViewModule 基本用法

--- viewmodule/src/main/java/com/example/viewmodule/MainViewModule.java
+++ viewmodule/src/main/java/com/example/viewmodule/MainViewModule.java
@@ -1,7 +1,24 @@
 package com.example.viewmodule;
 
+import android.util.Log;
+
 import androidx.lifecycle.ViewModel;
 
 public class MainViewModule extends ViewModel {
     private int counter = 0;
+
+    public int getCounter() {
+        Log.d("MainViewModuleTest", "getCounter"+counter);
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        Log.d("MainViewModuleTest", "setCounter"+counter);
+        this.counter = counter;
+    }
+
+    public void pushOneCounter() {
+        Log.d("MainViewModuleTest", "pushOneCounter"+counter);
+        this.counter += 1 ;
+    }
 }
